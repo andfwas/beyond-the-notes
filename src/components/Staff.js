b1503bcfafb9bfbaa6ec59f98f31e5d832de132b
andfwas <andfwas@gmail.com>
2018-01-27 22:17:04 -0700

    switching to staff branch to work on vexflow

--- src/components/Staff.js
+++ src/components/Staff.js
@@ -1,6 +1,28 @@
 import React from 'react'
+// import Vex from 'vexflow'
 import styled from 'styled-components'
 import staff from '../images/staff.png'
+
+// var VF = Vex.Flow;
+//
+// // Create an SVG renderer and attach it to the DIV element named "boo".
+// var div = document.getElementById("boo")
+// var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+//
+// // Size our svg:
+// renderer.resize(500, 500);
+//
+// // And get a drawing context:
+// var context = renderer.getContext();
+//
+// // Create a stave at position 10, 40 of width 400 on the canvas.
+// var Stave = new VF.Stave(10, 40, 400);
+//
+// // Add a clef and time signature.
+// Stave.addClef("treble").addTimeSignature("4/4");
+//
+// // Connect it to the rendering context and draw!
+// // Stave.setContext(context).draw();
 
 var Lesson = styled.img`
   width: 80%;
@@ -37,7 +59,6 @@ var ButtonDiv = styled.div`
   justify-content: center;
   background-color: #035460;
 `;
-
 class Staff extends React.Component {
   constructor() {
     super()
